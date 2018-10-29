9b0ea1542063f2c1d475bf0f4e81671b7dd33cbf
Nate Miller <natekmiller96@gmail.com>
2018-10-29 17:50:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var displayErrors = () => $('#errors').html("I'm sorry, there's been an error. Please try again.");
 
 var searchRepositories = () => {
-  const searchTerms = $('#searchTerms').val()
+  const searchTerms = $('#searchTerms').val();
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
       $('#results').html(renderSearchResults(data))
     }).fail(error => {
